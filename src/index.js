ec8aa996f216d19ea66393cba7fddd66a0822081
dmkolosov <maker.bug.00@inbox.ru>
2020-10-20 17:29:53 +0300

    feat:  add  a solution

--- src/index.js
+++ src/index.js
@@ -2,14 +2,58 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix=[]) {
+  if(!matrix.length){
+    return []
+  }
 
   let result=[]
 
+  matrix.forEach((element,index) => {
+    
+  if(index%2==0){
 
+  result.push(...element)}
+  else{
+    console.log(index,"even",element)
+    let aaa=element.reverse()
+    result.push(...aaa)
+  }
+  
+  });
+  console.log(result)
   return result
-
+//   return result.sort((a,b)=>a-b);
 }
 
+function towelSort (matrix) {
+  console.log('a',Array.isArray(matrix))
+  if(!matrix.length){
+    return []
+  }
+
+  let result=[]
+console.log(matrix)
+  matrix.forEach((element,index) => {
+    
+  if(index%2==0){
+    console.log(index,"odd",element,index%2==0)
+  result.push(...element)}
+  else{
+    console.log(index,"even",element)
+    let aaa=element.reverse()
+    result.push(...aaa)
+  }
+  
+  });
+  console.log(result)
+  return result
+}
+
+console.log(towelSort([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+]))
 
 
 
